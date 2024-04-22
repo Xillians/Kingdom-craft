6083175422487ca3185ea2daef5c8e92d97ed574
Xillians <sandra.toftemo@bjerk.io>
2024-04-22 18:09:48 +0200

    try cleaning up page

--- build/server/chunks/1-CX3ET2Sd.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-3Bf1Wlnb.js')).default;
-const imports = ["_app/immutable/nodes/1.BBc01UDL.js","_app/immutable/chunks/scheduler.C55nVr2J.js","_app/immutable/chunks/index.Bt9MCtE8.js","_app/immutable/chunks/singletons.PMoZowg0.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-CX3ET2Sd.js.map
--- server/chunks/0-BnyQJrAe.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-let component_cache;
-const component = async () => component_cache ??= (await import('./layout.svelte-B4spFuG6.js')).default;
-const imports = ["_app/immutable/nodes/0.ColSJjZ2.js","_app/immutable/chunks/scheduler.C55nVr2J.js","_app/immutable/chunks/index.Bt9MCtE8.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-BnyQJrAe.js.map
